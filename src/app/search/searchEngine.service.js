b2e003594d60b5a2763b574f86e750fa0d0e69a7
Jeroen Noten <jeroennoten@me.com>
2014-11-18 11:51:39 +0100

    search engine http request

--- src/app/search/searchEngine.service.js
+++ src/app/search/searchEngine.service.js
@@ -1,12 +1,24 @@
 angular.module('search')
     .factory('searchEngine', searchEngine);
 
-function searchEngine() {
+function searchEngine($http) {
     return {
         search: search
     };
 
     function search(query) {
-        console.log(query)
+        $http.get('https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/track.search', {
+            params: {
+                query: query
+            },
+            headers: {
+                'X-Mashape-Key': 'ImIOFOF9UimshzRZU3z2FHDt54tzp1TVWUojsn8fJ3hyg8GxKd'
+            }
+        })
+            .then(function (data) {
+                console.log(data);
+            }, function (error) {
+                console.log(error)
+            })
     }
 }
