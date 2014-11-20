11b353f637864870dd8dc1ca84974b6dd1ca5a88
Jeroen Noten <jeroennoten@me.com>
2014-11-20 11:45:50 +0100

    simple youtube video player from first search result

--- src/app/video/video.js
+++ src/app/video/video.js
@@ -1,7 +1,25 @@
 angular.module('vl.video')
     .controller('video', video);
 
-function video() {
+function video($http, $sce) {
     var ctrl = this;
-    console.log('video: ', ctrl.track.id);
+
+    var artist = ctrl.track.artist;
+    var title = ctrl.track.title;
+
+    $http.get('https://content.googleapis.com/youtube/v3/search', {
+        params: {
+            part: 'id',
+            type: 'video',
+            q: artist + ' ' + title,
+            videoEmbeddable: true,
+            key: 'AIzaSyAnAt5FyQ5UEv4nvLq83hD0UNCH0Ln-Sto',
+            maxResults: 1
+        }
+    })
+        .then(function (response) {
+            if (response.data.items.length > 0) {
+                ctrl.videoSrc = $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + response.data.items[0].id.videoId);
+            }
+        })
 }
