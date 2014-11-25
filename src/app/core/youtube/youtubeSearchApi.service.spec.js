f17054af50cf711b77c8e3c1dd3a51059dc7f926
Jeroen Noten <jeroennoten@me.com>
2014-11-25 13:15:56 +0100

    added comments to tests

--- src/app/core/youtube/youtubeSearchApi.service.spec.js
+++ src/app/core/youtube/youtubeSearchApi.service.spec.js
@@ -7,8 +7,10 @@ describe('youtubeSearchApi', function () {
 
         // inject dependencies
         inject(function (_youtubeSearchApi_, _youtubeSearchUri_, _$httpBackend_) {
+            // under test
             youtubeSearchApi = _youtubeSearchApi_;
             youtubeSearchUri = _youtubeSearchUri_;
+            // $httpBackend to mock the backend requests
             $httpBackend = _$httpBackend_;
         })
     });
@@ -25,7 +27,9 @@ describe('youtubeSearchApi', function () {
 
         it('returns the video ID via the promise', function () {
             var expectedVideoId = 12345;
+            // we except a GET request
             $httpBackend.expectGET(youtubeSearchUri + '?maxResults=1&part=id&q=dummy+query&type=video&videoEmbeddable=true')
+                // with status code 200 and JSON data
                 .respond(200, {
                     items: [{
                         id: {
@@ -34,12 +38,17 @@ describe('youtubeSearchApi', function () {
                     }]
                 });
 
+            // declare variable in this scope so that in can be used
             var actualVideoId;
+            // execute api request
             youtubeSearchApi.searchFirst('dummy query').then(function (videoId) {
+                // store the videoId
                 actualVideoId = videoId;
             });
 
+            // execute the outstanding requests
             $httpBackend.flush();
+
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
 
@@ -47,20 +56,25 @@ describe('youtubeSearchApi', function () {
         });
 
         it('returns an error via the promise when no video is found', function () {
+            // expect a GET request that returns no data
             $httpBackend.expectGET(youtubeSearchUri + '?maxResults=1&part=id&q=dummy+query&type=video&videoEmbeddable=true')
                 .respond(200, {
                     items: []
                 });
 
+            // store the returned error
             var actualError;
             youtubeSearchApi.searchFirst('dummy query').catch(function (error) {
                 actualError = error;
             });
 
+            // execute the outstanding requests
             $httpBackend.flush();
+
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
 
+            // actualError must be any instance of Error
             expect(actualError).toEqual(jasmine.any(Error));
         })
 
