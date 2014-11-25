3b58e3de114dacd12893d12d9190a77b24f5dc46
Vouwfietsman <tom_diggelen@hotmail.com>
2014-11-25 17:52:26 +0100

    Improved search and revamped HTML. Changed musixmatch service to spotify service via herokuapp. Since we no longer use musixmatch all ID's are changed and so all routing is broken.

---
+++ src/app/core/spotify.service.js
@@ -0,0 +1,63 @@
+// search service
+angular.module('vl.core')
+    .factory('spotifyAPI', spotifyAPI);
+
+function spotifyAPI($http, $q) {
+    var cache = {};
+    // public interface:
+    var service = {
+        searchTracks: APIsearchTracks,
+        search: APIsearch,
+        getTrack: getTrack
+    };
+
+    var baseURL = "https://video-lyrics.herokuapp.com/1.0/proxy/spotify/";
+
+    function APIgetTrack(id){
+         return $http.get(baseURL + 'tracks', {
+            params: {
+                id: id
+            },
+        }).then(function (data) {
+                cache[data.data.id] = data.data;
+                return data;
+            });
+    }
+
+      function APIsearch(name){
+         return $http.get(baseURL + 'search', {
+            params: {
+                q: name,
+                type: "album,artist",
+                limit:"6"
+            }
+        }).then(function (data) {
+                cache[data.data.track_id] = data.data;
+                return data;
+            });
+    }
+
+    function APIsearchTracks(name){
+         return $http.get(baseURL + 'search', {
+            params: {
+                q: name,
+                type: "track",
+                limit:"15"
+            }
+        }).then(function (data) {
+                cache[data.data.track_id] = data.data;
+                return data;
+            });
+    }
+
+
+    function getTrack(id){
+        if (cache[id]){
+            return $q.when(cache[id]);
+        }else{
+            return APIgetTrack(id);
+        }
+
+    }
+    return service;
+}
--- src/app/search/searchResults.js
+++ src/app/search/searchResults.js
@@ -2,14 +2,41 @@
 angular.module('vl.search')
     .controller('searchResults', searchResults);
 
-function searchResults($location, musixMatchAPI) {
+function searchResults($location, spotifyAPI) {
     var ctrl = this;
     ctrl.query = $location.search().q;
     console.log('henk');
-    musixMatchAPI.search(ctrl.query).then(function(data){
+    // musixMatchAPI.searchTracks(ctrl.query).then(function(data){
+    //     console.log('data');
+    //     console.log(data);
+    //     ctrl.tracks = data.data;
+    // });
+    spotifyAPI.search(ctrl.query).then(function(data){
         console.log('data');
         console.log(data);
-        ctrl.tracks = data.data;
-
+        ctrl.artists = data.data.artists.items;
+        findThumbnails(ctrl.artists);
+        ctrl.albums = data.data.albums.items;
+        findThumbnails(ctrl.albums);
     });
+    spotifyAPI.searchTracks(ctrl.query).then(function(data){
+        console.log('data');
+        console.log(data);
+        ctrl.tracks = data.data.tracks.items;
+    });
+}
+
+function findThumbnails(objects){
+    for (var index = 0; index < objects.length; index++){
+    	var obj = objects[index];
+    	obj.thumbnail = findThumbnail(obj.images);
+    }
+}
+
+function findThumbnail(images){
+	var smallest = images[images.length-1];
+	if (images.length == 0){
+		return "http://asset-d.soup.io/asset/1377/8322_d804_48-square.png";
+	}
+	return smallest.url;
 }
